f8f8636c1d8552ebfd86b1a92b4b3644ec41576a
Cam Way <cway140@gmail.com>
2022-12-02 13:50:32 -0800

    added 304 not modified

--- SingleThreadServer.py
+++ SingleThreadServer.py
@@ -2,15 +2,29 @@ from socket import *
 from utils import *
 import os
 import time
+from datetime import datetime
+import signal
 
+# define exceptions
+class BadRequest(Exception): pass
+class NotFound(Exception): pass
+class NotAllowed(Exception): pass
+class Timeout(Exception): pass
 
 def parse_headers(request):
-    headers = {}
-
-    return headers
+    try:
+        headers = {}
+        for header in request:
+            if header == "":
+                break
+            key, value = header.split(": ")
+            headers[key] = value
+        return headers
+    except:
+        raise BadRequest
 
 def get_headers(status, body):
-    headers = "HTTP/1.1" + status + "\r\n"
+    headers = "HTTP/1.1 " + status + "\r\n"
     headers += "Content-Type: text/html\r\n"
     headers += "Content-Length: " + str(len(body)) + "\r\n"
 
@@ -20,7 +34,6 @@ def construct_http_packet(status, body = ""):
     headers = get_headers(status, body)
     response = headers + "\r\n" + body
 
-    print("Responding with: " + status)
     return response
 
 def get_file(path):
@@ -29,52 +42,86 @@ def get_file(path):
         f = open(path, "r")
         file = f.read()
         f.close()
+        time.sleep(5)
         return file
     else:
-        return None
+        raise NotFound
 
+# checks if requested file has been modified since the last time it was requested
+# return True if it has been modified, False otherwise
+# returns True if If-Modified-Since header is not present
 def is_modified_since(headers, path):
-    return False
+    if "If-Modified-Since" in headers:
+        headerTime = headers["If-Modified-Since"]
+        path = path[1:] # remove the leading slash
 
+        mtime = os.path.getmtime(path)
+
+        # convert headerTime to datetime object
+        headerTime = datetime.strptime(headerTime, "%a, %d %b %Y %H:%M:%S %Z")
+        mtime = datetime.fromtimestamp(mtime)
+
+        if mtime < headerTime:
+            return False
+
+    return True
+
+def timout_handler(signum, frame):
+    raise Timeout
 
 # SETUP SERVER
 serverName = 'localhost'
 serverPort = 12000
+timeout = 1
 serverSocket = socket(AF_INET, SOCK_STREAM)
 serverSocket.bind((serverName, serverPort))
 
 serverSocket.listen(1)
 print('Listening on port', serverPort)
+
+# register timeout handler
+signal.signal(signal.SIGALRM, timout_handler)
+
 while True:
-        connectionSocket, addr = serverSocket.accept()
-        request = connectionSocket.recv(1024).decode()
-        try:
-                request_words = request.split()
-                method = request_words[0]
-                path = request_words[1]
-                print("Handling " + method + " request on path " + path)
-                
-                if method == 'GET':
-                        # parse headers
-                        response_body = get_file(path)
-                        if response_body:
-                            response = construct_http_packet("200 OK", response_body)
-                        else: 
-                            response = construct_http_packet("404 Not Found", "<h1>404 Not Found</h1>")
-                else:
-                        response = construct_http_packet("405 Method Not Allowed")
-                        
-                connectionSocket.send(response.encode())
-        except Exception as e:
-                print(e)
-                response = construct_http_packet("500 Internal Server Error")
+    connectionSocket, addr = serverSocket.accept()
+    request = connectionSocket.recv(1024).decode()
+    signal.alarm(timeout)
         
-        connectionSocket.close()
+    try:
+            request_words = request.split()
+            request_lines = request.splitlines()
+            method = request_words[0]
+            path = request_words[1]
+            print("Handling " + method + " request on path " + path)
+            
+            if method == 'GET':
+                    headers = parse_headers(request_lines[1:])
+                    if is_modified_since(headers, path):
+                        response_body = get_file(path)
+                        response = construct_http_packet("200 OK", response_body)
+                    else:
+                        response = construct_http_packet("304 Not Modified")
+            else:
+                   raise NotAllowed
+
+    except NotAllowed:
+            response = construct_http_packet("405 Method Not Allowed")
+    except NotFound:
+            response = construct_http_packet("404 Not Found", "<h1>404 Not Found</h1>")
+    except Timeout:
+            response = construct_http_packet("408 Request Timeout")
+    except Exception as e:
+            print(e)
+            response = construct_http_packet("500 Internal Server Error")
+   
+    print("Responding with status code " + response.split()[1])
+    connectionSocket.send(response.encode())
+    connectionSocket.close()
 
 
 ### TODO
 #[X] 200 	OK
-#[] 304 	Not Modified
-#[] 400 	Bad request
-#[#] 404 	Not Found
+#[X] 304 	Not Modified
+#[X] 400 	Bad request
+#[X] 404 	Not Found
 #[] 408 	Request Timed Out
